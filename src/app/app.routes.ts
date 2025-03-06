806d7311c833d52ce42719294364f1c26b762262
hammerSecond <holacuentaburner@gmail.com>
2025-03-06 16:28:13 +0100

    actualizadas las rutas incluyendo un home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path:"",
-        component: MainContentComponent,
-        title: "Main Content",
-    },
-    {
-        path:"register",
-        component: RegisterFormComponent,
-        title: "Register Form",
-    }
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'home',
+    component: MainContentComponent,
+    title: 'Main Content',
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent,
+    title: 'Register Form',
+  },
 ];
