f8d3e996fd6f074570073c71c4b4bb77804fc239
hammerSecond <holacuentaburner@gmail.com>
2025-04-07 19:05:36 +0200

    minor changes

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-register-form',
+  selector: 'register-form',
   imports: [RouterModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
